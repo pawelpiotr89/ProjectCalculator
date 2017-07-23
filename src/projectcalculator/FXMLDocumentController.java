12f8c18a92a7d4346fc4dac4e5f38df7393fffff
Paweł J. <roxven@gmail.com>
2017-07-23 19:38:44 +0200

    4 - project adjustments

--- src/projectcalculator/FXMLDocumentController.java
+++ src/projectcalculator/FXMLDocumentController.java
@@ -14,7 +14,7 @@ import javafx.fxml.Initializable;
  * @author Roxven89
  */
 public class FXMLDocumentController implements Initializable {
-  
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- src/projectcalculator/ProjectCalculator.java
+++ src/projectcalculator/ProjectCalculator.java
@@ -1,25 +1,46 @@
 package projectcalculator;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 /**
  *
  * @author Roxven89
  */
 public class ProjectCalculator extends Application {
+
+    private Stage mainStage;
+    private Scene welcomeScene;
+    private Pane welcomePane;
+    private Label welcomeLabel, authorLabel;
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root1 = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
-        Scene scene = new Scene(root1);
+        mainStage = stage;
+        mainStage.setTitle("PROJECT_CALCULATOR v. 1.0");
         
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
+        initWelcomeLayout();
+    }
+    
+    public void initWelcomeLayout(){
+        try{
+            FXMLLoader welcomeLayoutLoader = new FXMLLoader();
+            welcomeLayoutLoader.setLocation(ProjectCalculator.class.getResource("FXMLDocument.fxml"));
+            welcomePane = (Pane) welcomeLayoutLoader.load();
+            
+            welcomeScene = new Scene(welcomePane);
+            mainStage.setScene(welcomeScene);
+            mainStage.setResizable(false);
+            mainStage.show();
+            
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
     
     /**
