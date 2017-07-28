1c8c4fd2beb304452206f31f9631449943a218f3
Paweł J. <roxven@gmail.com>
2017-07-28 22:28:13 +0200

    16 - project adjustments

--- src/projectCalculatorControllers/CostCenterPaneController.java
+++ src/projectCalculatorControllers/CostCenterPaneController.java
@@ -1,8 +1,10 @@
 package projectCalculatorControllers;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
-
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -10,56 +12,70 @@ import javafx.scene.control.TitledPane;
  * @author Roxven89
  */
 public class CostCenterPaneController {
-    
+
+    private MainPaneController mainPaneController;
+
     @FXML
     private TitledPane materialCostTitledPane;
-    
+
     @FXML
     private TitledPane subconstructorsCostTitledPane;
-    
+
     @FXML
     private TitledPane labourCostTitledPane;
-    
+
     @FXML
     private TitledPane logisticCostTitledPane;
-    
+
     @FXML
     private TitledPane otherCostTitledPane;
-    
-    @FXML
-    private TitledPane backToMenuTitledPane;
-    
+
     @FXML
     private TitledPane addNewMaterialCostTitledPane;
-    
+
     @FXML
     private TitledPane changeMaterialCostTitledPane;
-    
+
     @FXML
     private TitledPane displayMaterialCostTitledPane;
-    
+
     @FXML
     private TitledPane removeMaterialCostTitledPane;
-    
+
     @FXML
-    private void onMouseReleased(){
-        if(addNewMaterialCostTitledPane.isExpanded() || 
-           changeMaterialCostTitledPane.isExpanded() || 
-           displayMaterialCostTitledPane.isExpanded() ||
-           removeMaterialCostTitledPane.isExpanded()) {
-           materialCostTitledPane.setCollapsible(false);
-           subconstructorsCostTitledPane.setCollapsible(false);
-           labourCostTitledPane.setCollapsible(false);
-           logisticCostTitledPane.setCollapsible(false);
-           otherCostTitledPane.setCollapsible(false);
-           backToMenuTitledPane.setCollapsible(false);
+    private void onMouseReleased() {
+        if (addNewMaterialCostTitledPane.isExpanded()
+                || changeMaterialCostTitledPane.isExpanded()
+                || displayMaterialCostTitledPane.isExpanded()
+                || removeMaterialCostTitledPane.isExpanded()) {
+            materialCostTitledPane.setCollapsible(false);
+            subconstructorsCostTitledPane.setCollapsible(false);
+            labourCostTitledPane.setCollapsible(false);
+            logisticCostTitledPane.setCollapsible(false);
+            otherCostTitledPane.setCollapsible(false);
         } else {
-           materialCostTitledPane.setCollapsible(true);
-           subconstructorsCostTitledPane.setCollapsible(true);
-           labourCostTitledPane.setCollapsible(true);
-           logisticCostTitledPane.setCollapsible(true);
-           otherCostTitledPane.setCollapsible(true);
-           backToMenuTitledPane.setCollapsible(true);
+            materialCostTitledPane.setCollapsible(true);
+            subconstructorsCostTitledPane.setCollapsible(true);
+            labourCostTitledPane.setCollapsible(true);
+            logisticCostTitledPane.setCollapsible(true);
+            otherCostTitledPane.setCollapsible(true);
         }
-    }          
+    }
+
+    @FXML
+    public void backToMenu() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/projectCalculatorsFXML/FXMLMenuPane.fxml"));
+        Pane pane = null;
+        try {
+            pane = loader.load();
+        } catch (IOException e) {
+        }
+        MenuPaneController menuPaneController = loader.getController();
+        menuPaneController.setMainPaneController(mainPaneController);
+        mainPaneController.setPane(pane);
+    }
+
+    public void setMainPaneController(MainPaneController mainPaneController) {
+        this.mainPaneController = mainPaneController;
+    }
 }
