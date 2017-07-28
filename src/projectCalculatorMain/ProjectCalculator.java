491c358bab1e5da1e896a2f50f344d56619ed46d
Paweł J. <roxven@gmail.com>
2017-07-28 22:19:03 +0200

    15 - project adjustments

--- src/projectCalculatorMain/ProjectCalculator.java
+++ src/projectCalculatorMain/ProjectCalculator.java
@@ -1,6 +1,5 @@
 package projectCalculatorMain;
 
-import java.awt.Color;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
@@ -9,51 +8,59 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import projectCalculatorControllers.MainPaneController;
+
 /**
  *
  * @author Roxven89
  */
 public class ProjectCalculator extends Application {
 
-    private Stage mainStage;
+    private static Stage mainStage;
     private Scene mainScene;
     private Pane mainPane;
     private Cursor cursor;
     private ExitAlertWindow exitAlertWindow;
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        
-        mainStage = stage;
+
+        setPrimaryStage(stage);
         cursor = Cursor.DEFAULT;
-     
+
         FXMLLoader mainPaneLoader = new FXMLLoader();
         mainPaneLoader.setLocation(ProjectCalculator.class.getResource("/projectCalculatorsFXML/FXMLMainPane.fxml"));
         mainPane = mainPaneLoader.load();
         MainPaneController mainPaneController = mainPaneLoader.getController();
-        
+
         mainScene = new Scene(mainPane);
         mainScene.setCursor(cursor);
-        
-        mainStage.setOnCloseRequest(e ->{
+
+        mainStage.setOnCloseRequest(e -> {
             e.consume();
             exitAlertWindow = new ExitAlertWindow();
             exitAlertWindow.askingQuestion();
         });
-        
+
         mainStage.setTitle("PROJECT_CALCULATOR v. 1.0");
         mainStage.initStyle(StageStyle.UTILITY);
         mainStage.setResizable(false);
         mainStage.setScene(mainScene);
         mainStage.show();
     }
-    
-    
+
+    private void setPrimaryStage(Stage stage) {
+        ProjectCalculator.mainStage = stage;
+    }
+
+    static public Stage getPrimaryStage() {
+        return mainStage;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
-    
+
     }
 }
